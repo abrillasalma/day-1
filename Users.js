ec4446ff209a17755efd1fb0b8f83d3df8eeb003
abrilla <abrilla.salma@gmail.com>
2021-07-02 09:09:59 +0700

    Initial commit

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: 1,
+    name: 'John',
+    email: 'john@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    email: 'jane@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Isaiah',
+    email: 'isaiah@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Meriwether',
+    email: 'meriwether@gmail.com',
+  },
+];
+
+module.exports = users;
